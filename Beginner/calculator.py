136a3665fca14f1987a87adca8102e5bacea8047
callouse <callousekarki@gmail.com>
2025-02-20 10:59:31 +0545

    Added the ablity of choice, with implementation of loops and proper error handling

--- Beginner/calculator.py
+++ Beginner/calculator.py
@@ -1,9 +1,4 @@
-import math
-
-x = int(input("Enter the first number: "))
-y = int(input("Enter the second number: "))
-
-def sum(x, y):
+def add(x, y):
     return x + y 
 
 def diff(x, y): 
@@ -13,9 +8,51 @@ def mult(x, y):
     return x * y
 
 def div(x, y): 
+    if y == 0: 
+        return "Error: Cannot divide by zero!"
     return x/y
 
-print(f"The addition is, {sum(x,y)}")
-print(f"The subtraction is, {diff(x,y)}")
-print(f"The multiplication is, {mult(x,y)}")
-print(f"The division is, {div(x,y)}")
+print("Select the operation: ")
+print("1. ADDITION")
+print("2. SUBTRACTION")
+print("3. MULTIPLICATION")
+print("4. DIVISION")
+
+
+while True: 
+        
+    choice = input("Enter your operation (1/2/3/4): ")
+    
+    if choice in ('1', '2', '3', '4'): 
+        try: 
+            x = float(input("Enter the first number: "))
+            y = float(input("Enter the second number: "))
+                
+        except ValueError: 
+            print("Invalid Input!")
+            continue
+
+        if choice == '1': 
+            print(f"The additon is, {add(x,y)}")
+            
+        if choice == '2': 
+            print(f"The subtraction is, {diff(x,y)}")
+            
+        if choice == '3': 
+            print(f"The multiplication is, {mult(x,y)}")
+
+        if choice == '4': 
+            result = div(x,y)
+            if isinstance(result, str): 
+                print(result)
+            else: 
+                print(f"The division is, {result}")
+            
+        again = input("Do you want to run it again? (y/n): ").lower()
+        
+        if again != 'y': 
+            print("Thankyou for using the calculator application :)")
+            break
+        
+    else: 
+        print("Invalid Input! Enter operations from (1/2/3/4)")
